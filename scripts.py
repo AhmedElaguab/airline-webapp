60f29e110c7250798c4ffe2ab216766f19e9f6f3
Ahmed Elaguab <elaguabahmed97@gmail.com>
2020-06-18 18:55:06 +0100

    Add import flights script

--- scripts.py
+++ scripts.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask
+import csv
 from models import *
 
 app = Flask(__name__)
@@ -7,9 +8,23 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
 
 
+def import_flights():
+    """This function is used to import flights into database."""
+    f = open("data/flights.csv")
+    reader = csv.reader(f)
+    for origin, destination, duration in reader:
+        flight = Flight(origin=origin, destination=destination,
+                        duration=duration)
+        db.session.add(flight)
+    db.session.commit()
+
+
 def main():
-    """This script is used to create your 'SQL' tables"""
+    """This script is used to create your 'SQL' tables."""
     db.create_all()
+
+    # Uncomment the function call bellow to import flights into database
+    # import_flights()
 
 
 if __name__ == "__main__":
