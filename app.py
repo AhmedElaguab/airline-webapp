d177e81c3c4d8c0ef94f124adf5ab234d55caaf2
Ahmed Elaguab <elaguabahmed97@gmail.com>
2020-06-22 18:29:46 +0100

    Add book flight route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from models import *
 
 app = Flask(__name__)
@@ -13,6 +13,20 @@ def index():
     """List flights for booking."""
     flights = Flight.query.all()
     return render_template("index.html", flights=flights)
+
+
+@app.route("/book", methods=["POST"])
+def book():
+    """Book a flight."""
+
+    # Get name input value
+    name = request.form.get("name")
+
+    # Validate name value
+    if name == None or len(name) < 3:
+        return render_template("error.html", message="Name value must be more than 2 characters.")
+
+    return "book a flight"
 
 
 @app.route("/flights")
